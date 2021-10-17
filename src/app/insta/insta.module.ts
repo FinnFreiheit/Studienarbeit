a7b9cc7748700e4039e5d3634db7f4b514444059
FinnFreiheit <finn.owe.freiheit@gmail.com>
2021-10-17 21:06:16 +0200

    carousel

--- src/app/insta/insta.module.ts
+++ src/app/insta/insta.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InstaContainerComponent } from './insta-container/insta-container.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { IconsComponent } from './icons/icons.component';
 
 
 
 @NgModule({
   declarations: [
-    InstaContainerComponent
+    InstaContainerComponent,
+    PicturesComponent,
+    IconsComponent
   ],
   imports: [
     CommonModule
