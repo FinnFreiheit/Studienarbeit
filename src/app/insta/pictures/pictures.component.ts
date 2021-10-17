db512728dd2bcc96e7d0c6e2479209fc7ddd3ceb
FinnFreiheit <finn.owe.freiheit@gmail.com>
2021-10-17 21:37:01 +0200

    check carousel

--- src/app/insta/pictures/pictures.component.ts
+++ src/app/insta/pictures/pictures.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PicturesComponent implements OnInit {
 
+
+  public pictures = [
+    { src: "../../../assets/pic01.jpg"},
+    { src: "../../../assets/pic02.jpg"},
+    { src: "../../../assets/pic03.jpg"}
+  ];
+
+  currentPic = 0; 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onPreviousClick(){
+    const previous = this.currentPic -1 ; 
+    this.currentPic = previous < 0 ? this.pictures.length - 1: previous; 
+    console.log("previous clicked, new current pic is: ", this.currentPic);
+  }
+
+  onNextClick() {
+    const next = this.currentPic + 1;
+    this.currentPic = next === this.pictures.length ? 0: next;
+    console.log("next clicked, new current pic is: ", this.currentPic)
   }
 
 }
