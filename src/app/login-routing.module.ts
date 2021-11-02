01d4dd89d0e545ca9279aee599f52c079b52a4b9
Finn Freiheit <finnfreiheit@MacBook-Pro-von-Finn.local>
2021-11-02 10:36:35 +0100

    signUp

--- src/app/login-routing.module.ts
+++ src/app/login-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login/login.component";
+import {SignUpComponent} from "./login/sign-up/sign-up.component";
 
 
 const loginRoutes: Routes = [
-  { path: 'page/login', component: LoginComponent }
+  { path: 'page/login', component: LoginComponent },
+  { path: 'page/login/signUp', component: SignUpComponent},
+
 ];
 
 @NgModule({
